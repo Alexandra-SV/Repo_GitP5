301253df11dfe82419dff30a1945054ffba5d4a7
Alexandra-SV <alexandraclases1707@gmail.com>
2023-05-12 15:45:12 +0200

    Commit Ej16

---
+++ src/Ejs/Rectangulo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Ejs;
+
+/**
+ * @author Alexandra SV
+ */
+public class Rectangulo {
+    float alto, ancho;
+    public Rectangulo(float l1, float l2){
+        alto = l1;
+        ancho = l2;
+    }
+}
